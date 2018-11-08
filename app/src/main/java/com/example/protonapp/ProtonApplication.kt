71e9362e17c56b343690721408cb50588efa2c3f
Adam Szkup <szkup.a@gmail.com>
2018-11-08 09:11:54 +0100

    add ReleaseTree

--- app/src/main/java/com/example/protonapp/ProtonApplication.kt
+++ app/src/main/java/com/example/protonapp/ProtonApplication.kt
@@ -3,20 +3,23 @@ package com.example.protonapp
 import com.android.base.BaseApplication
 import com.example.protonapp.di.repositoryModule
 import com.example.protonapp.di.viewModelModule
+import com.example.protonapp.utils.ReleaseTree
 import com.facebook.stetho.Stetho
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.KodeinAware
 import com.github.salomonbrys.kodein.lazy
 import com.jakewharton.threetenabp.AndroidThreeTen
 import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 class ProtonApplication : BaseApplication(), KodeinAware {
 
     override fun onCreate() {
         super.onCreate()
 
+        if (BuildConfig.DEBUG) Timber.plant(DebugTree()) else Timber.plant(ReleaseTree())
         AndroidThreeTen.init(this)
-        Timber.plant(Timber.DebugTree())
         Stetho.initializeWithDefaults(this)
     }
 
