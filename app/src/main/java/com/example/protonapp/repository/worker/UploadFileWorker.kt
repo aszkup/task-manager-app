7d44aa71ddfaa2b42994ffda06043caa8afbdd97
Adam Szkup <szkup.a@gmail.com>
2018-11-24 16:21:58 +0100

    simplify uploadWorker, implement workaround for Work Interrupted bug

--- app/src/main/java/com/example/protonapp/repository/worker/UploadFileWorker.kt
+++ app/src/main/java/com/example/protonapp/repository/worker/UploadFileWorker.kt
@@ -16,7 +16,8 @@ import com.example.protonapp.repository.task.Task
 import com.example.protonapp.repository.task.TaskRepository
 import com.example.protonapp.utils.FileUtils
 import com.example.protonapp.utils.WorkManagerUtils.Companion.WORK_REQUEST_TAG
-import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
 import org.kodein.di.KodeinAware
 import org.kodein.di.generic.instance
 import timber.log.Timber
@@ -37,7 +38,7 @@ class UploadFileWorker(
     private val contentResolver: ContentResolver by instance()
     private val fileUtils: FileUtils by instance()
     private val notificationHelper: NotificationHelper by instance()
-    private var disposable: Disposable? = null
+    private val compositeDisposable = CompositeDisposable()
     private var notificationId: Int? = null
     private var dropBoxUploadUploader: UploadUploader? = null
     private var task: Task? = null
@@ -46,15 +47,21 @@ class UploadFileWorker(
     override fun doWork(): Result {
         Timber.i("Worker ${this::class.java.simpleName} started.")
         inputData.getString(WORK_REQUEST_TAG)?.let { taskId ->
-            disposable = repository.getTask(taskId)
+            repository.getTask(taskId)
                     .doOnSuccess { Timber.d("Task to process $it") }
                     .doOnSuccess { this.task = it }
                     .map { sendFile(it) }
                     .subscribe({ }, { Timber.e(it) })
+                    .addTo(compositeDisposable)
             return ListenableWorker.Result.SUCCESS
         }
         return ListenableWorker.Result.FAILURE
     }
+
+    //TODO WorkManager is in alpha11 version so this workaround is needed. Its quite often when
+    // `Work interrupted` is observed after `Work Success` and `onStopped()` is called.
+    // If upload will be finished properly, then avoid canceling Task and notification
+    var isUploadFinishedSuccessfully = false
 
     override fun onStopped() {
         Timber.d("On Stopped")
@@ -64,32 +71,59 @@ class UploadFileWorker(
         } catch (exception: Exception) {
             Timber.e(exception)
         }
-        notificationId?.let { notificationHelper.markNotificationCanceled(it) }
-        disposable?.dispose()
-        task?.let { repository.cancelTask(it).subscribe({}, { Timber.e(it) }) }
+        if (!isUploadFinishedSuccessfully) {
+            notificationId?.let { notificationHelper.markNotificationCanceled(it) }
+            task?.let {
+                repository.cancelTask(it)
+                        .subscribe({}, { Timber.e(it) })
+                        .addTo(compositeDisposable)
+            }
+        }
+        compositeDisposable.dispose()
     }
 
     @Throws(FileNotFoundException::class, IOException::class)
     private fun sendFile(task: Task): Task {
-        var task = task
         Timber.d("File Uri: ${task.fileUri}")
         val fileDescriptor = contentResolver.openFileDescriptor(Uri.parse(task.fileUri), READ_MODE)
         fileDescriptor?.let {
             val stream = FileInputStream(fileDescriptor.fileDescriptor)
             val fileName = fileUtils.getFileName(Uri.parse(task.fileUri))
-            Timber.i("File to upload: $fileName")
-            repository.startTask(task).subscribe({ task = it }, { Timber.e(it) })
-            notificationId = notificationHelper.createNotification(task.id, task.name, fileName)
-            dropBoxUploadUploader = dropboxClient.files().uploadBuilder(DROP_BOX_DESTINATION + fileName)
-                    .withMode(WriteMode.OVERWRITE)
-                    .start()
-            dropBoxUploadUploader?.uploadAndFinish(stream, getProgressListener(it.statSize))
 
-            notificationId?.let { notificationHelper.markNotificationFinished(it) }
-            Timber.i("Upload completed: $fileName")
-            repository.finishTask(task).subscribe({ task = it }, { Timber.e(it) })
+            beforeUpload(fileName)
+            uploadFile(fileName, stream, it.statSize)
+            uploadCompleted(fileName)
         }
         return task
+    }
+
+    private fun beforeUpload(fileName: String) {
+        Timber.i("File to upload: $fileName")
+        task?.let { task ->
+            notificationId = notificationHelper.createNotification(task.id, task.name, fileName)
+            repository.startTask(task)
+                    .subscribe({ this.task = it }, { Timber.e(it) })
+                    .addTo(compositeDisposable)
+        }
+    }
+
+    private fun uploadFile(fileName: String, stream: FileInputStream, fileSize: Long) {
+        dropBoxUploadUploader = dropboxClient.files()
+                .uploadBuilder(DROP_BOX_DESTINATION + fileName)
+                .withMode(WriteMode.OVERWRITE)
+                .start()
+        dropBoxUploadUploader?.uploadAndFinish(stream, getProgressListener(fileSize))
+    }
+
+    private fun uploadCompleted(fileName: String) {
+        Timber.i("Upload completed: $fileName")
+        notificationId?.let { notificationHelper.markNotificationFinished(it) }
+        task?.let { task ->
+            repository.finishTask(task)
+                    .subscribe({ this.task = it }, { Timber.e(it) })
+                    .addTo(compositeDisposable)
+        }
+        isUploadFinishedSuccessfully = true
     }
 
     private fun getProgressListener(fileSize: Long) =
